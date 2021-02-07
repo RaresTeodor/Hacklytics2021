301d83680b1c7dcb0653e0eb865b12b15d53c76c
toagabi <gabriel.toader@protonmail.com>
2021-02-07 15:51:43 +0200

    added pictures to stack

--- Frontend/myapp/lib/View/FirstPage/Ranking.dart
+++ Frontend/myapp/lib/View/FirstPage/Ranking.dart
@@ -1,13 +1,17 @@
 import 'dart:html';
 
 import 'package:flutter/cupertino.dart';
-import 'package:myapp/View/FirstPage/MatchResultTable.dart';
 
 class Ranking extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: MatchResultTable(),
+      child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return Text("Stats");
+          }),
     );
   }
 }
