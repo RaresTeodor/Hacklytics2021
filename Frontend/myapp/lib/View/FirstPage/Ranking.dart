9dcb8230838cba43744b4c5c56adc79ba59de2b0
toagabi <gabriel.toader@protonmail.com>
2021-02-07 14:16:34 +0200

    Add rankings

---
+++ Frontend/myapp/lib/View/FirstPage/Ranking.dart
@@ -0,0 +1,13 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:myapp/View/FirstPage/MatchResultTable.dart';
+
+class Ranking extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MatchResultTable(),
+    );
+  }
+}
