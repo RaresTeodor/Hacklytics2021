f3b595a1660a1fdd8f26f409768343d0e76c7f27
RaresTeodor <78275422+RaresTeodor@users.noreply.github.com>
2021-02-07 11:20:45 +0000

    Worked on the design of the front page

--- Frontend/myapp/lib/View/FirstPage/FirstPage.dart
+++ Frontend/myapp/lib/View/FirstPage/FirstPage.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'MatchResultPage.dart';
 
@@ -10,102 +8,96 @@ class FirstPage extends StatelessWidget {
     //   HttpRequest.getString('127.0.0.1/names')
     //       .then((result) => {result.split("\n,")});
     // } as List<String>;
-
     return new Container(
-        color: Colors.lightBlue,
         child: Column(
+      children: [
+        Text("Welcome to Futer UFC"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text("Welcome to Futer UFC"),
-            Row(
+            //Fighter 1
+            Column(
               children: [
-                //Fighter 1
-                Column(
-                  children: [
-                    //name
-                    Container(
-                      child: TextFormField(
-                        controller: TextEditingController(),
-                        autofocus: false,
-                        decoration: InputDecoration(
-                            icon: Icon(Icons.people),
-                            hintText: "Enter your fighter",
-                            labelText: "Figher A"),
-                        //autofillHints: names,
-                      ),
-                      width: 400,
-                      height: 400,
-                    )
-                  ],
-                )
+                //name
+                Container(
+                  child: TextFormField(
+                    controller: TextEditingController(),
+                    autofocus: false,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.people),
+                        hintText: "Enter your fighter",
+                        labelText: "Figher A"),
+                    //autofillHints: names,
+                  ),
+                  width: 400,
+                  height: 100,
+                ),
+                Container(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Image.asset(
+                      'assets/images/randy.png',
+                      scale: 3,
+                    ))
               ],
             ),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MatchResultPage()));
-                },
-                child: Text("Rumble!")),
+            Column(
+              children: [
+                Container(
+                    padding: EdgeInsets.only(bottom: 50),
+                    child: Image.asset(
+                      'assets/images/randy.png',
+                      scale: 3,
+                    )),
+                Container(
+                    margin: EdgeInsets.only(bottom: 50),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MatchResultPage()));
+                      },
+                      child: Text('Rumble'),
+                    )),
+                Container(
+                    child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MatchResultPage()));
+                  },
+                  child: Text('Ranking'),
+                ))
+              ],
+            ),
+            Column(
+              children: [
+                //name
+                Container(
+                  child: TextFormField(
+                    controller: TextEditingController(),
+                    autofocus: false,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.people),
+                        hintText: "Enter your fighter",
+                        labelText: "Figher B"),
+                    //autofillHints: names,
+                  ),
+                  width: 400,
+                  height: 100,
+                ),
+                Container(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Image.asset(
+                      'assets/images/randy.png',
+                      scale: 3,
+                    ))
+              ],
+            )
           ],
-        ));
+        ),
+      ],
+    ));
   }
 }
-
-// class FirstPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Scaffold(body: new Item());
-//   }
-// }
-
-// class Item extends StatelessWidget {
-//   final left = new Container(
-//     padding: new EdgeInsets.only(left: 8.0),
-//     child: new TextField(
-//       obscureText: true,
-//       decoration: InputDecoration(
-//         border: OutlineInputBorder(),
-//         labelText: 'Password',
-//       ),
-//     ),
-//   );
-
-//   final middle = new Container(
-//       padding: new EdgeInsets.only(left: 8.0),
-//       child: new TextButton(
-//         onPressed: () {
-//           /*...*/
-//         },
-//         child: Text(
-//           "Insert",
-//           style: TextStyle(fontSize: 18.0),
-//         ),
-//       ));
-
-//   final right = new Container(
-//     padding: new EdgeInsets.only(left: 8.0),
-//     child: new TextField(
-//       obscureText: true,
-//       decoration: InputDecoration(
-//         border: OutlineInputBorder(),
-//         labelText: 'Password',
-//       ),
-//     ),
-//   );
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Scaffold(
-//       body: new Container(
-//         child: new Row(
-//           children: [
-//             left,
-//             middle,
-//             right,
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
